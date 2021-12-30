6ec4f400027b0678e94c6b087dba9419642e40b1
LJunius <1253023731@qq.com>
2021-12-30 14:04:55 +0800

    fix:database create

--- init_database.py
+++ init_database.py
@@ -18,6 +18,71 @@ def insert_into_table(table_name, values):
     db.close()
 
 
+user_table = '''create table user(
+    id int primary key not null,
+    username text not null, -- 注册必备
+    password text not null, -- 注册必备
+    phone_num text not null, -- 注册必备
+    description char(100),
+    user_type int not null, -- 1：普通用户 2：管理员 管理员只能后台注册
+    identity_type int not null, -- 1：身份证 2：护照 注册必备
+    identity_num text not null, -- 注册必备
+    real_name text not null, -- 注册必备
+    level int, -- 1-2等级逐渐增大
+    city text not null, -- 注册必备
+    community text not null, --注册必备
+    signup_time text not null,
+    modify_time text not null
+);'''
+
+callup_table = '''create table callup(
+    id              integer         primary key     autoincrement,
+    callup_user_id         integer         not null,
+    name            text            not null,
+    type            integer         not null,
+    description     text            not null,
+    member          integer         not null,
+    end_time        text            not null,
+    img             text            not null,
+    create_time     text            not null,
+    modify_time     text            not null,
+    state           integer         not null,       -- 1:已完成 2:待响应 3:已取消 4:到期未达成
+    foreign key (callup_user_id) references user(id) on update cascade on delete cascade
+);'''
+
+callup_response = '''create table callup_response(
+    id              integer         primary key     autoincrement,
+    callup_id       integer         not null,
+    response_user_id         integer         not null,
+    description     text            not null,
+    create_time     text            not null,
+    modify_time     text            not null,
+    state           integer         not null,       -- 1:待处理 2:同意 3:拒绝 4:取消
+    foreign key (callup_id) references callup(id) on update cascade on delete cascade, 
+    foreign key (response_user_id) references user(id) on update cascade on delete cascade
+);'''
+
+
+callup_success = '''create table callup_success(
+    id              integer         primary key     autoincrement,
+    callup_user_id       integer    not null,
+    response_user_id     integer    not null,
+    finish_time     text            not null,
+    callup_fee      integer         not null,
+    response_fee    integer         not null,
+    foreign key (callup_user_id) references user(id) on update cascade on delete cascade
+    foreign key (response_user_id) references user(id) on update cascade on delete cascade
+);'''
+
+agency_earning = '''create table agency_earning(
+    id              integer         primary key     autoincrement,
+    month           text            not null,
+    city            text            not null,
+    community       text            not null,
+    type            integer         not null,
+    finish_nums     int             not null,
+    earning_fee     int             not null,
+);'''
 
 
 user_Admin = '(1, "admin", "admin", "12345678910", "I am admin", 2, 1, 500227111111111111,"lj", 2, "beijing","Community1", "2020-12-5", "2020-12-5")'
